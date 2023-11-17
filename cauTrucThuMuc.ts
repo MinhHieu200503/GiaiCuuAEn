93315ad588cfbd22ba61268b04a63b6cefe76eba
Hieu Lai <ldmhieudev@gmail.com>
2023-11-17 15:28:23 +0700

    Chat gpt

---
+++ cauTrucThuMuc.ts
@@ -0,0 +1,17 @@
+// my-redux-app/
+// |-- public/
+// |-- src/
+//     |-- actions/
+//         |-- dataActions.js
+//     |-- components/
+//         |-- DataComponent.js
+//     |-- reducers/
+//         |-- dataReducer.js
+//         |-- index.js
+//     |-- store/
+//         |-- index.js
+//     |-- App.js
+//     |-- index.js
+//     |-- index.css
+// |-- package.json
+// |-- ...
