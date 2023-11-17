db85479ddfa36cc121545d8d05442b055161063f
Hieu Lai <ldmhieudev@gmail.com>
2023-11-17 15:58:45 +0700

    CRUD todolist và cau trúc thư mục

--- cau2.ts
+++ cau2.ts
@@ -95,3 +95,114 @@ export default TodoList;
 Trong đoạn mã trên, chúng ta sử dụng hook useDispatch để gửi action đến Redux Store và useSelector để lấy dữ liệu từ Redux Store.
 
 Lưu ý: Đảm bảo rằng bạn đã khởi động máy chủ Node.js (chứa API) và điều chỉnh URL trong axios.get phản ánh URL của máy chủ của bạn.
+
+
+// ==========================================================================================================================================================
+                                                                                            // CRUD TodoList
+// Để thêm, xóa, và sửa các công việc trong danh sách Todo sử dụng Redux và Redux Toolkit, bạn cần thực hiện một số bước. Dưới đây là một hướng dẫn cơ bản:
+
+// Sửa Redux Slice (todoSlice.js):
+// jsx
+
+// slices/todoSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    setTodos: (state, action) => {
+      return action.payload;
+    },
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteTodo: (state, action) => {
+      return state.filter(todo => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, updatedTodo } = action.payload;
+      const index = state.findIndex(todo => todo.id === id);
+      if (index !== -1) {
+        state[index] = updatedTodo;
+      }
+    },
+  },
+});
+
+export const { setTodos, addTodo, deleteTodo, updateTodo } = todoSlice.actions;
+export default todoSlice.reducer;
+// Tạo Component để thêm, xóa, và sửa công việc (TodoList.js):
+// jsx
+// Copy code
+// TodoList.js
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, TextInput, Button } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTodos, addTodo, deleteTodo, updateTodo } from './slices/todoSlice';
+import axios from 'axios';
+
+const TodoList = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector(state => state.todos);
+
+  const [newTodo, setNewTodo] = useState('');
+  const [editTodo, setEditTodo] = useState({ id: null, text: '' });
+
+  useEffect(() => {
+    // Fetch data from the API
+    axios.get('http://localhost:3000/api/todos')
+      .then(response => dispatch(setTodos(response.data)))
+      .catch(error => console.error('Error fetching data:', error));
+  }, [dispatch]);
+
+  const handleAddTodo = () => {
+    const todo = { id: Date.now(), todojob: newTodo };
+    dispatch(addTodo(todo));
+    setNewTodo('');
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  const handleEditTodo = () => {
+    dispatch(updateTodo({ id: editTodo.id, updatedTodo: { id: editTodo.id, todojob: editTodo.text } }));
+    setEditTodo({ id: null, text: '' });
+  };
+
+  return (
+    <View>
+      <Text>Todo List:</Text>
+      <TextInput
+        placeholder="New Todo"
+        value={newTodo}
+        onChangeText={(text) => setNewTodo(text)}
+      />
+      <Button title="Add" onPress={handleAddTodo} />
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.todojob}</Text>
+            <Button title="Edit" onPress={() => setEditTodo({ id: item.id, text: item.todojob })} />
+            <Button title="Delete" onPress={() => handleDeleteTodo(item.id)} />
+          </View>
+        )}
+      />
+      {editTodo.id !== null && (
+        <View>
+          <TextInput
+            placeholder="Edit Todo"
+            value={editTodo.text}
+            onChangeText={(text) => setEditTodo({ ...editTodo, text })}
+          />
+          <Button title="Save" onPress={handleEditTodo} />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default TodoList;
---
+++ cauTrucThuMuc.ts
@@ -0,0 +1,18 @@
+// Cấu trúc thư mục trong một ứng dụng React Native sử dụng Redux và Redux Toolkit có thể được tổ chức theo nhiều cách khác nhau, tùy thuộc vào kích thước và phức tạp của ứng dụng của bạn. Dưới đây là một cấu trúc thư mục cơ bản:
+
+// css
+// Copy code
+// myApp
+// │
+// ├── src
+// │   ├── components
+// │   │   └── TodoList.js
+// │   ├── slices
+// │   │   └── todoSlice.js
+// │   ├── store
+// │   │   └── store.js
+// │   └── App.js
+// │
+// ├── package.json
+// ├── node_modules
+// └── ...
