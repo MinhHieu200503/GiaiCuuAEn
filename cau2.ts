6572c7c935b06aabf78d541104c2f8c1d86a68a2
Hieu Lai <ldmhieudev@gmail.com>
2023-12-08 00:50:48 +0700

    cau 2

--- cau2.ts
+++ cau2.ts
@@ -1,208 +1,199 @@
-Để tích hợp Redux và Redux Toolkit vào ứng dụng React Native và hiển thị dữ liệu từ API lên màn hình, bạn cần thực hiện một số bước cơ bản. Dưới đây là một hướng dẫn đơn giản:
+// Dưới đây là một ví dụ cách thiết kế màn hình Screen_02 và thực hiện các yêu cầu bạn đã nêu, sử dụng React Native Expo và Redux Toolkit:
 
-Cài đặt thư viện Redux và Redux Toolkit:
-bash
-Copy code
-npm install @reduxjs/toolkit react-redux
-Tạo Redux Slice:
-Tạo một Redux Slice để quản lý trạng thái của danh sách công việc.
-
-jsx
-Copy code
-// slices/todoSlice.js
+// Cài đặt Redux Toolkit:
+// bash
+// Copy code
+//     npm install @reduxjs/toolkit react-redux
+// Tạo file userSlice.js trong thư mục redux/slices:
+// javascript
+// Copy code
+// redux/slices/userSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
-const todoSlice = createSlice({
-  name: 'todos',
-  initialState: [],
-  reducers: {
-    setTodos: (state, action) => {
-      return action.payload;
+const userSlice = createSlice({
+    name: 'user',
+    initialState: { id: 0, password: '', todos: [] },
+    reducers: {
+        setUser: (state, action) => {
+            return action.payload;
+        },
+        addTodo: (state, action) => {
+            state.todos.push(action.payload);
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo !== action.payload);
+        },
     },
-  },
 });
 
-export const { setTodos } = todoSlice.actions;
-export default todoSlice.reducer;
-Tạo Redux Store:
-jsx
-Copy code
-// store.js
+export const { setUser, addTodo, deleteTodo } = userSlice.actions;
+export default userSlice.reducer;
+// Sửa đổi file userReducer.js trong thư mục redux/reducers:
+// javascript
+// Copy code
+// redux/reducers/userReducer.js
+import { combineReducers } from 'redux';
+import userReducer from '../slices/userSlice';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+export default rootReducer;
+// Sửa đổi file store.js trong thư mục redux:
+// javascript
+// Copy code
+// redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from './slices/todoSlice';
+import rootReducer from './reducers/userReducer';
 
 const store = configureStore({
-  reducer: {
-    todos: todoReducer,
-  },
+    reducer: rootReducer,
 });
 
 export default store;
-Kết nối Redux Store với ứng dụng chính:
-jsx
-Copy code
+// Sửa đổi file Screen_01.js để sử dụng Redux Toolkit:
+// javascript
+// Copy code
+// Screen_01.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { setUser } from './redux/slices/userSlice';
+import { loginUser } from './api';
+
+const Screen_01 = ({ navigation }) => {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const user = await loginUser(email, password);
+
+            if (user) {
+                dispatch(setUser(user)); // Lưu thông tin user vào Redux store
+                Alert.alert('Login successful!', 'Redirecting to Screen 02...');
+                // Thêm logic chuyển màn hình tại đây
+                // Ví dụ: navigation.navigate('Screen_02');
+            } else {
+                Alert.alert('Invalid email or password', 'Please try again.');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+
+    return (
+        <View>
+            <Text>Login</Text>
+            <TextInput
+                placeholder="Enter your email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+                placeholder="Enter your password"
+                secureTextEntry
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+            />
+            <Button title="Continue" onPress={handleLogin} />
+        </View>
+    );
+};
+
+export default Screen_01;
+// Tạo file Screen_02.js để hiển thị thông tin và thêm/xóa công việc:
+// javascript
+// Copy code
+// Screen_02.js
+import React, { useState } from 'react';
+import { View, Text, FlatList, TextInput, Button, Alert } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+import { addTodo, deleteTodo } from './redux/slices/userSlice';
+
+const Screen_02 = () => {
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const [newTodo, setNewTodo] = useState('');
+
+    const handleAddTodo = () => {
+        if (newTodo.trim() !== '') {
+            dispatch(addTodo(newTodo));
+            setNewTodo('');
+        } else {
+            Alert.alert('Error', 'Please enter a valid todo.');
+        }
+    };
+
+    const handleDeleteTodo = (todo) => {
+        dispatch(deleteTodo(todo));
+    };
+
+    return (
+        <View>
+            <Text>User ID: {user.id}</Text>
+            <Text>Password: {user.password}</Text>
+            <Text>Todos:</Text>
+            <FlatList
+                data={user.todos}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <View>
+                        <Text>{item}</Text>
+                        <Button
+                            title="Delete"
+                            onPress={() => handleDeleteTodo(item)}
+                        />
+                    </View>
+                )}
+            />
+            <TextInput
+                placeholder="Add a new todo"
+                value={newTodo}
+                onChangeText={(text) => setNewTodo(text)}
+            />
+            <Button title="Add" onPress={handleAddTodo} />
+        </View>
+    );
+};
+
+export default Screen_02;
+// Sửa đổi file App.js để thêm màn hình Screen_02:
+// javascript
+// Copy code
 // App.js
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store';
-import TodoList from './TodoList';
+import { createStore, combineReducers } from 'redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Screen_01 from './Screen_01';
+import Screen_02 from './Screen_02';
+import userReducer from './redux/reducers/userReducer';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const store = createStore(rootReducer);
+
+const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <Provider store={store}>
-      <TodoList />
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Screen_01">
+                    <Stack.Screen name="Screen_01" component={Screen_01} />
+                    <Stack.Screen name="Screen_02" component={Screen_02} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
 };
 
 export default App;
-Tạo Component hiển thị danh sách công việc:
-jsx
-Copy code
-// TodoList.js
-import React, { useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTodos } from './slices/todoSlice';
-import axios from 'axios';
-
-const TodoList = () => {
-  const dispatch = useDispatch();
-  const todos = useSelector(state => state.todos);
-
-  useEffect(() => {
-    // Fetch data from the API
-    axios.get('http://localhost:3000/api/todos')
-      .then(response => dispatch(setTodos(response.data)))
-      .catch(error => console.error('Error fetching data:', error));
-  }, [dispatch]);
-
-  return (
-    <View>
-      <Text>Todo List:</Text>
-      <FlatList
-        data={todos}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <Text>{item.todojob}</Text>
-        )}
-      />
-    </View>
-  );
-};
-
-export default TodoList;
-Trong đoạn mã trên, chúng ta sử dụng hook useDispatch để gửi action đến Redux Store và useSelector để lấy dữ liệu từ Redux Store.
-
-Lưu ý: Đảm bảo rằng bạn đã khởi động máy chủ Node.js (chứa API) và điều chỉnh URL trong axios.get phản ánh URL của máy chủ của bạn.
-
-
-// ==========================================================================================================================================================
-                                                                                            // CRUD TodoList
-// Để thêm, xóa, và sửa các công việc trong danh sách Todo sử dụng Redux và Redux Toolkit, bạn cần thực hiện một số bước. Dưới đây là một hướng dẫn cơ bản:
-
-// Sửa Redux Slice (todoSlice.js):
-// jsx
-
-// slices/todoSlice.js
-import { createSlice } from '@reduxjs/toolkit';
-
-const todoSlice = createSlice({
-  name: 'todos',
-  initialState: [],
-  reducers: {
-    setTodos: (state, action) => {
-      return action.payload;
-    },
-    addTodo: (state, action) => {
-      state.push(action.payload);
-    },
-    deleteTodo: (state, action) => {
-      return state.filter(todo => todo.id !== action.payload);
-    },
-    updateTodo: (state, action) => {
-      const { id, updatedTodo } = action.payload;
-      const index = state.findIndex(todo => todo.id === id);
-      if (index !== -1) {
-        state[index] = updatedTodo;
-      }
-    },
-  },
-});
-
-export const { setTodos, addTodo, deleteTodo, updateTodo } = todoSlice.actions;
-export default todoSlice.reducer;
-// Tạo Component để thêm, xóa, và sửa công việc (TodoList.js):
-// jsx
-// Copy code
-// TodoList.js
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TextInput, Button } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTodos, addTodo, deleteTodo, updateTodo } from './slices/todoSlice';
-import axios from 'axios';
-
-const TodoList = () => {
-  const dispatch = useDispatch();
-  const todos = useSelector(state => state.todos);
-
-  const [newTodo, setNewTodo] = useState('');
-  const [editTodo, setEditTodo] = useState({ id: null, text: '' });
-
-  useEffect(() => {
-    // Fetch data from the API
-    axios.get('http://localhost:3000/api/todos')
-      .then(response => dispatch(setTodos(response.data)))
-      .catch(error => console.error('Error fetching data:', error));
-  }, [dispatch]);
-
-  const handleAddTodo = () => {
-    const todo = { id: Date.now(), todojob: newTodo };
-    dispatch(addTodo(todo));
-    setNewTodo('');
-  };
-
-  const handleDeleteTodo = (id) => {
-    dispatch(deleteTodo(id));
-  };
-
-  const handleEditTodo = () => {
-    dispatch(updateTodo({ id: editTodo.id, updatedTodo: { id: editTodo.id, todojob: editTodo.text } }));
-    setEditTodo({ id: null, text: '' });
-  };
-
-  return (
-    <View>
-      <Text>Todo List:</Text>
-      <TextInput
-        placeholder="New Todo"
-        value={newTodo}
-        onChangeText={(text) => setNewTodo(text)}
-      />
-      <Button title="Add" onPress={handleAddTodo} />
-      <FlatList
-        data={todos}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.todojob}</Text>
-            <Button title="Edit" onPress={() => setEditTodo({ id: item.id, text: item.todojob })} />
-            <Button title="Delete" onPress={() => handleDeleteTodo(item.id)} />
-          </View>
-        )}
-      />
-      {editTodo.id !== null && (
-        <View>
-          <TextInput
-            placeholder="Edit Todo"
-            value={editTodo.text}
-            onChangeText={(text) => setEditTodo({ ...editTodo, text })}
-          />
-          <Button title="Save" onPress={handleEditTodo} />
-        </View>
-      )}
-    </View>
-  );
-};
-
-export default TodoList;
+// Lưu ý rằng trong ví dụ này, các công việc được lưu trữ trong mảng todos của Redux store. Bạn có thể tùy chỉnh và mở rộng logic để đáp ứng đúng với yêu cầu cụ thể của ứng dụng của mình.
