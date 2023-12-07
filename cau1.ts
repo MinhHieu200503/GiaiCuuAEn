858df3e2ec57dba7f3013d5eaf592d2ae39d5d44
Hieu Lai <ldmhieudev@gmail.com>
2023-12-08 00:48:00 +0700

    cau 1

--- cau1.ts
+++ cau1.ts
@@ -1,67 +1,140 @@
-Để tạo một API đơn giản với hai trường dữ liệu là id và todojob và sau đó sử dụng nó để hiển thị dữ liệu trong ứng dụng Native, bạn cần thực hiện một số bước sau:
+// Để sử dụng Redux trong ứng dụng React Native với Expo, bạn cần cài đặt thư viện react-redux và redux. Dưới đây là hướng dẫn chi tiết:
 
-Tạo API:
-Bạn có thể sử dụng một số công cụ như Node.js và Express để tạo một API đơn giản. Dưới đây là một ví dụ đơn giản bằng Node.js:
+// Bắt đầu một dự án Expo mới:
+// bash
+// Copy code
+// expo init ReduxApp
+// cd ReduxApp
+// Cài đặt thư viện Redux và React Redux:
+// bash
+// Copy code
+// npm install redux react-redux
+// Tạo các thư mục và file cho Redux:
+// Tạo thư mục redux và bên trong tạo các thư mục actions, reducers.
+// Tạo file types.js trong thư mục actions:
+// javascript
+// Copy code
+// actions/types.js
+export const SET_USER = 'SET_USER';
+// Tạo file userActions.js trong thư mục actions:
+// javascript
+// Copy code
+// actions/userActions.js
+import { SET_USER } from './types';
 
-javascript
-Copy code
-// server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-app.use(cors());
-
-let todoList = [];
-
-app.get('/api/todos', (req, res) => {
-  res.json(todoList);
+export const setUser = (user) => ({
+    type: SET_USER,
+    payload: user,
 });
+// Tạo file userReducer.js trong thư mục reducers:
+// javascript
+// Copy code
+// reducers/userReducer.js
+import { SET_USER } from '../actions/types';
 
-app.post('/api/todos', (req, res) => {
-  const { id, todojob } = req.body;
-  const newTodo = { id, todojob };
-  todoList.push(newTodo);
-  res.json(newTodo);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-Gửi dữ liệu từ ứng dụng Native:
-Trong ứng dụng Native của bạn, bạn sẽ cần sử dụng một thư viện HTTP client để gửi yêu cầu đến API. Dưới đây là một ví dụ sử dụng fetch:
-
-javascript
-Copy code
-// NativeApp.js
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-
-const NativeApp = () => {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from the API
-    fetch('http://localhost:3000/api/todos')
-      .then(response => response.json())
-      .then(data => setTodos(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-
-  return (
-    <View>
-      <Text>Todo List:</Text>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>{todo.todojob}</li>
-        ))}
-      </ul>
-    </View>
-  );
+const initialState = {
+    user: null,
 };
 
-export default NativeApp;
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER:
+            return {
+                ...state,
+                user: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
+// Kết nối Redux với ứng dụng:
+// Sửa file App.js để kết nối Redux:
+// javascript
+// Copy code
+// // App.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Screen_01 from './Screen_01';
+import userReducer from './redux/reducers/userReducer';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const store = createStore(rootReducer);
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Screen_01">
+                    <Stack.Screen name="Screen_01" component={Screen_01} />
+                    {/* Thêm các màn hình khác tại đây nếu cần */}
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
+};
+
+export default App;
+// Sửa đổi Screen_01.js để sử dụng Redux:
+// javascript
+// Copy code
+// Screen_01.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { setUser } from './redux/actions/userActions';
+import { loginUser } from './api';
+
+const Screen_01 = ({ navigation }) => {
+    const dispatch = useDispatch();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const user = await loginUser(email, password);
+
+            if (user) {
+                dispatch(setUser(user)); // Lưu thông tin user vào Redux store
+                Alert.alert('Login successful!', 'Redirecting to Screen 02...');
+                // Thêm logic chuyển màn hình tại đây
+                // Ví dụ: navigation.navigate('Screen_02');
+            } else {
+                Alert.alert('Invalid email or password', 'Please try again.');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    };
+
+    return (
+        <View>
+            <Text>Login</Text>
+            <TextInput
+                placeholder="Enter your email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+                placeholder="Enter your password"
+                secureTextEntry
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+            />
+            <Button title="Continue" onPress={handleLogin} />
+        </View>
+    );
+};
+
+export default Screen_01;
+// Trong ví dụ này, Redux được sử dụng để lưu trữ thông tin người dùng sau khi đăng nhập, và useDispatch hook được sử dụng để gửi hành động setUser đến Redux store. Ngoài ra, bạn có thể sử dụng useSelector hook để truy cập thông tin từ Redux store nếu cần thiết.
