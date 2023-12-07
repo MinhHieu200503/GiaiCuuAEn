68deaf31f5f5b059086d77ade28778a459d46834
Hieu Lai <ldmhieudev@gmail.com>
2023-12-08 01:05:22 +0700

    cau 1 chinh sua => ts

--- cau1.ts
+++ cau1.ts
@@ -1,140 +1,200 @@
-// Để sử dụng Redux trong ứng dụng React Native với Expo, bạn cần cài đặt thư viện react-redux và redux. Dưới đây là hướng dẫn chi tiết:
+// Để sử dụng React Native Expo, Redux và TypeScript để tạo màn hình Screen_01, bạn có thể làm theo các bước sau:
 
-// Bắt đầu một dự án Expo mới:
+// Khởi tạo Dự án Expo TypeScript:
+
 // bash
 // Copy code
-// expo init ReduxApp
-// cd ReduxApp
-// Cài đặt thư viện Redux và React Redux:
+// expo init MyProject --template expo-template-blank-typescript
+// cd MyProject
+// Cài đặt thư viện Redux và Axios:
+
 // bash
 // Copy code
-// npm install redux react-redux
-// Tạo các thư mục và file cho Redux:
-// Tạo thư mục redux và bên trong tạo các thư mục actions, reducers.
-// Tạo file types.js trong thư mục actions:
-// javascript
+// npm install redux react-redux axios
+// Tạo thư mục redux và tạo các file cho Redux:
+
+// Trong thư mục src, tạo thư mục redux.
+// Trong thư mục redux, tạo các thư mục actions, reducers, slices.
+// Trong thư mục redux, tạo file store.ts:
+// typescript
 // Copy code
-// actions/types.js
-export const SET_USER = 'SET_USER';
-// Tạo file userActions.js trong thư mục actions:
-// javascript
-// Copy code
-// actions/userActions.js
-import { SET_USER } from './types';
+// src/redux/store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './slices/userSlice';
 
-export const setUser = (user) => ({
-    type: SET_USER,
-    payload: user,
-});
-// Tạo file userReducer.js trong thư mục reducers:
-// javascript
-// Copy code
-// reducers/userReducer.js
-import { SET_USER } from '../actions/types';
-
-const initialState = {
-    user: null,
-};
-
-const userReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SET_USER:
-            return {
-                ...state,
-                user: action.payload,
-            };
-        default:
-            return state;
-    }
-};
-
-export default userReducer;
-// Kết nối Redux với ứng dụng:
-// Sửa file App.js để kết nối Redux:
-// javascript
-// Copy code
-// // App.js
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Screen_01 from './Screen_01';
-import userReducer from './redux/reducers/userReducer';
-
-const rootReducer = combineReducers({
+const store = configureStore({
+  reducer: {
     user: userReducer,
+  },
 });
 
-const store = createStore(rootReducer);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
-const Stack = createStackNavigator();
+export default store;
+// Trong thư mục redux, tạo file slices/userSlice.ts:
+// typescript
+// Copy code
+// src/redux/slices/userSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const App = () => {
-    return (
-        <Provider store={store}>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="Screen_01">
-                    <Stack.Screen name="Screen_01" component={Screen_01} />
-                    {/* Thêm các màn hình khác tại đây nếu cần */}
-                </Stack.Navigator>
-            </NavigationContainer>
-        </Provider>
-    );
+interface User {
+  id: number;
+  password: string;
+  todos: string[];
+}
+
+const initialState: User = {
+  id: 0,
+  password: '',
+  todos: [],
 };
 
-export default App;
-// Sửa đổi Screen_01.js để sử dụng Redux:
-// javascript
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
+// Tạo màn hình Screen_01 sử dụng React Navigation và Redux:
+
+// Cài đặt React Navigation:
+// bash
 // Copy code
-// Screen_01.js
+// npm install @react-navigation/native @react-navigation/stack
+// Trong thư mục src, tạo file screens/Screen_01.tsx:
+// tsx
+// Copy code
+// src/screens/Screen_01.tsx
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Alert } from 'react-native';
 import { useDispatch } from 'react-redux';
-import { setUser } from './redux/actions/userActions';
-import { loginUser } from './api';
+import { setUser } from '../redux/slices/userSlice';
+import { NavigationProp } from '@react-navigation/native';
+import { RootStackParamList } from '../types/navigation';
 
-const Screen_01 = ({ navigation }) => {
-    const dispatch = useDispatch();
+interface Screen01Props {
+  navigation: NavigationProp<RootStackParamList, 'Screen_01'>;
+}
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const Screen_01: React.FC<Screen01Props> = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleLogin = async () => {
-        try {
-            const user = await loginUser(email, password);
+  const handleLogin = async () => {
+    try {
+      // Gọi API để kiểm tra thông tin đăng nhập
+      // (Sử dụng API mock trong trường hợp này)
+      const response = await fetch('https://6145c71638339400175fc557.mockapi.io/users');
+      const users = await response.json();
+      const user = users.find(u => u.password === password && u.id.toString() === email);
 
-            if (user) {
-                dispatch(setUser(user)); // Lưu thông tin user vào Redux store
-                Alert.alert('Login successful!', 'Redirecting to Screen 02...');
-                // Thêm logic chuyển màn hình tại đây
-                // Ví dụ: navigation.navigate('Screen_02');
-            } else {
-                Alert.alert('Invalid email or password', 'Please try again.');
-            }
-        } catch (error) {
-            console.error('Error during login:', error);
-        }
-    };
+      if (user) {
+        dispatch(setUser(user)); // Lưu thông tin user vào Redux store
+        Alert.alert('Login successful!', 'Redirecting to Screen 02...');
+        navigation.navigate('Screen_02');
+      } else {
+        Alert.alert('Invalid email or password', 'Please try again.');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
 
-    return (
-        <View>
-            <Text>Login</Text>
-            <TextInput
-                placeholder="Enter your email"
-                value={email}
-                onChangeText={(text) => setEmail(text)}
-            />
-            <TextInput
-                placeholder="Enter your password"
-                secureTextEntry
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-            />
-            <Button title="Continue" onPress={handleLogin} />
-        </View>
-    );
+  return (
+    <View>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Enter your email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        keyboardType="numeric"
+      />
+      <TextInput
+        placeholder="Enter your password"
+        secureTextEntry
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <Button title="Continue" onPress={handleLogin} />
+    </View>
+  );
 };
 
 export default Screen_01;
-// Trong ví dụ này, Redux được sử dụng để lưu trữ thông tin người dùng sau khi đăng nhập, và useDispatch hook được sử dụng để gửi hành động setUser đến Redux store. Ngoài ra, bạn có thể sử dụng useSelector hook để truy cập thông tin từ Redux store nếu cần thiết.
+// Tạo file screens/Screen_02.tsx:
+// tsx
+// Copy code
+// src/screens/Screen_02.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+
+const Screen_02: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user);
+
+  return (
+    <View>
+      <Text>User ID: {user.id}</Text>
+      <Text>Password: {user.password}</Text>
+      <Text>Todos: {JSON.stringify(user.todos)}</Text>
+    </View>
+  );
+};
+
+export default Screen_02;
+// Tạo file types/navigation.ts để định nghĩa kiểu cho React Navigation:
+// typescript
+// Copy code
+// src/types/navigation.ts
+import { StackNavigationProp } from '@react-navigation/stack';
+
+export type RootStackParamList = {
+  Screen_01: undefined;
+  Screen_02: undefined;
+};
+
+export type Screen_01NavigationProp = StackNavigationProp<RootStackParamList, 'Screen_01'>;
+Cập nhật file src/App.tsx để sử dụng React Navigation:
+tsx
+Copy code
+// src/App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Provider } from 'react-redux';
+import Screen_01 from './screens/Screen_01';
+import Screen_02 from './screens/Screen_02';
+import store from './redux/store';
+import { RootStackParamList } from './types/navigation';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Screen_01">
+          <Stack.Screen name="Screen_01" component={Screen_01} />
+          <Stack.Screen name="Screen_02" component={Screen_02} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default App;
+// Chắc chắn rằng bạn đã cài đặt Expo và TypeScript trước khi bắt đầu thực hiện các bước trên.
+
+
+
+
+
